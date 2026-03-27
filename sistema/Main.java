622c5b5289e14989324305c1981ad36d6a4be4f1
Oscar <ocastillob3@miumg.edu.gt>
2026-03-27 01:12:01 -0600

    Encapsulamiento y modificadores de acceso

--- sistema/Main.java
+++ sistema/Main.java
@@ -1,1 +1,19 @@
 package sistema;
+
+//3. ENUMS
+enum Dia {
+LUNES, MARTES, MIERCOLES, JUEVES, VIERNES
+}
+
+//8. ENCAPSULATION + 7. ACCESS SPECIFIERS
+class PersonaEncapsulada {
+private String nombre; // atributo privado (access modifier)
+
+public void setNombre(String nombre) { // setter
+   this.nombre = nombre;
+}
+
+public String getNombre() { // getter
+   return nombre;
+}
+}
--- sistema/package-info.java
+++ sistema/package-info.java
@@ -1,13 +1,1 @@
 package sistema;
-
-class Estudiante {
-    String nombre;
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Estudiante e = new Estudiante();
-        e.nombre = "Oscar";
-        System.out.println(e.nombre);
-    }
-}
